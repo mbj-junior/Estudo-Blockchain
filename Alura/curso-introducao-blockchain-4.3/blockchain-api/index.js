0a9af5acc880b46e0afc4cb380315730172ff2ec
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-20 19:27:08 -0300

    Material da aula

--- Alura/curso-introducao-blockchain-4.3/blockchain-api/index.js
+++ Alura/curso-introducao-blockchain-4.3/blockchain-api/index.js
@@ -2,21 +2,20 @@ var restify = require('restify');
 
 const {registerBlockchain} = require('./sawtooth/client');
 
-function registerVote(req, res, next){
-    const voto = req.body;
+function registerVote(req, res, next) {
+  const voto = req.body;
+  registerBlockchain(voto);
 
-    registerBlockchain(voto);
-
-    res.send(200);
-    next;
+  res.send(200);
+  next();
 }
 
 var server = restify.createServer();
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.bodyParser());
 
-server.post('register/vote', registerVote);
+server.post('/register/vote', registerVote);
 
 server.listen(8084, function() {
-    console.log('%s listening at %s', server.name, server.url);
+  console.log('%s listening at %s', server.name, server.url);
 });
